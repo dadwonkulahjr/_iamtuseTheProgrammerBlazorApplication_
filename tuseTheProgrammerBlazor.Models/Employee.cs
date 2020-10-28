a163f049ce311cba2a34c90d88c25aeda2807cb1
Dad S Wonkulah Jr <68390101+dadwonkulahjr@users.noreply.github.com>
2020-10-28 18:01:26 +0000

    Initial Commit blazor app dev

---
+++ tuseTheProgrammerBlazor.Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tuseTheProgrammerBlazor.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhotoPath { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Department Department { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
---
+++ tuseTheProgrammerBlazorApplication/Pages/EmployeeListBase.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using tuseTheProgrammerBlazor.Models;
+
+namespace tuseTheProgrammerBlazorApplication.Pages
+{
+    public class EmployeeListBase : ComponentBase
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+        protected override Task OnInitializedAsync()
+        {
+            LoadAllEmployees();
+            return base.OnInitializedAsync();
+        }
+        private void LoadAllEmployees()
+        {
+            Employee emp1 = new Employee()
+            {
+                EmployeeId = 101,
+                FirstName = "David",
+                LastName = "James",
+                Email = "david@tuseTheProgrammer.com",
+                BirthDate = new DateTime(1980, 12, 10),
+                Department = new Department() { DepartmentId = 1, DepartmentName = "IT" },
+                Gender = Gender.Male,
+                PhotoPath = "images/david.png"
+            };
+
+            Employee emp2 = new Employee()
+            {
+                EmployeeId = 102,
+                FirstName = "Sara",
+                LastName = "Peters",
+                Email = "sara@tuseTheProgrammer.com",
+                BirthDate = new DateTime(1990, 10, 05),
+                Department = new Department() { DepartmentId = 2, DepartmentName = "HR" },
+                Gender = Gender.Female,
+                PhotoPath = "images/sara.png"
+            };
+
+            Employee emp3 = new Employee()
+            {
+                EmployeeId = 103,
+                FirstName = "Freya",
+                LastName = "Holland",
+                Email = "freya@tuseTheProgrammer.com",
+                BirthDate = new DateTime(1996, 05, 11),
+                Department = new Department() { DepartmentId = 3, DepartmentName = "MGT" },
+                Gender = Gender.Female,
+                PhotoPath = "images/freya.png"
+            };
+
+            Employee emp4 = new Employee()
+            {
+                EmployeeId = 104,
+                FirstName = "Prince",
+                LastName = "Solo",
+                Email = "price@tuseTheProgrammer.com",
+                BirthDate = new DateTime(1985, 08, 10),
+                Department = new Department() { DepartmentId = 4, DepartmentName = "IT" },
+                Gender = Gender.Male,
+                PhotoPath = "images/car.jpg"
+            };
+
+
+            List<Employee> listOfEmployees = new List<Employee>()
+           {
+               emp1, emp2, emp3, emp4
+           };
+
+            Employees = listOfEmployees;
+        }
+    }
+}
